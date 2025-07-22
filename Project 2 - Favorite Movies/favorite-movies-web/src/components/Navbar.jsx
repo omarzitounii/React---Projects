dd4a644c5735c9f88da186ccfae3df1dcec481de
Omar Zitouni <zitouniomar2000@gmail.com>
2025-07-22 19:55:42 +0200

    Updated screenshots

--- Project 2 - Favorite Movies/favorite-movies-web/src/components/Navbar.jsx
+++ Project 2 - Favorite Movies/favorite-movies-web/src/components/Navbar.jsx
@@ -6,12 +6,12 @@ import { faSkullCrossbones } from '@fortawesome/free-solid-svg-icons';
 const Navbar = () => {
   const location = useLocation();
   return (
-    <nav className='fixed top-0 left-0 right-0 bg-black text-rose-700 text-xl sm:text-2xl font-bold py-5 border-b-2 shadow-2xl z-50'>
+    <nav className='fixed top-0 left-0 right-0 bg-black text-white text-xl sm:text-2xl font-bold py-5 border-b-2 border-rose-700 shadow-2xl z-50'>
       <div className='max-w-screen-2xl mx-auto px-4 flex justify-between items-center'>
-        <Link to="/"><FontAwesomeIcon icon={faSkullCrossbones} className="text-3xl sm:text-5xl hover:text-white active:scale-95 transition-all duration-200" /></Link>
+        <Link to="/"><FontAwesomeIcon icon={faSkullCrossbones} className="text-3xl sm:text-5xl text-rose-700 hover:text-white active:scale-95 transition-all duration-200" /></Link>
         <ul className='flex flex-row gap-6'>
-          <li className={`${location.pathname === '/' ? 'text-white' : ''} hover:text-white active:scale-95 transition-all duration-200`}><Link to="/">Home</Link></li>
-          <li className={`${location.pathname === '/favorites' ? 'text-white' : ''} hover:text-white active:scale-95 transition-all duration-200`}><Link to="/favorites">Favorite</Link></li>
+          <li className={`${location.pathname === '/' ? 'text-rose-700' : ''}  active:scale-95 transition-all duration-200`}><Link to="/">Home</Link></li>
+          <li className={`${location.pathname === '/favorites' ? 'text-rose-700' : ''} active:scale-95 transition-all duration-200`}><Link to="/favorites">Favorite</Link></li>
         </ul>
       </div>
     </nav>
